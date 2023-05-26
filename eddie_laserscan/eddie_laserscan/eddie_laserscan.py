0bef1501b85015f5c21bd68d5b7ee7ad7d56c50c
Mohammad Hossein Hosseini <smh@mail.um.ac.ir>
2023-05-26 16:23:54 +0330

    lab8 succeeded

--- eddie_laserscan/eddie_laserscan/eddie_laserscan.py
+++ eddie_laserscan/eddie_laserscan/eddie_laserscan.py
@@ -12,6 +12,7 @@ import cv2
 import numpy as np
 import time
 
+
 class Eddie_laserscan(Node):
     def __init__(self):
         super().__init__('eddie_laserscan')
@@ -60,32 +61,69 @@ class Eddie_laserscan(Node):
         }
 
         if bgr_image[:, :, 0].max() < 25:
-            self.get_logger().info(f"no more blue now") 
-            if bgr_image[:, :, 1].max() < 25: # no green
-                self.get_logger().info(f"red") 
-            elif bgr_image[:, :, 2].max() < 25: # no red
-                self.get_logger().info(f"green") 
+            if bgr_image[:, :, 1].max() < 25: # not green -> red
+                if self.state not in [3,33]:
+                    self.get_logger().info(f"red detected") 
+                    self.state = 3
+                    self.pivot_start = True
+
+            elif bgr_image[:, :, 2].max() < 25: # not red -> green
+                if self.state not in [4,44]:
+                    self.get_logger().info(f"green detected") 
+                    self.state = 4
+                    self.pivot_start = True
+
             else: #not red & green -> yellow
                 # self.get_logger().info(f"yellow") 
                 if self.state not in [2,22]:
+                    self.get_logger().info(f"yellow detected") 
                     self.state = 2
+                    self.pivot_start = True
                     
             
             
 
-        # Iterate over each pixel in the image
-        # for row in bgr_image:
-        #     for pixel in row:
-        #         # Check if the pixel's color falls within any of the color ranges
-        #         for color, (lower, upper) in color_ranges.items():
-        #             # if cv2.inRange(np.array(pixel), np.array(lower), np.array(upper)):
-        #             #     color_counts[color] += 1
+        if self.state == 1 and self.pivot_start:
+        
+            # Iterate over each pixel in the image
+            left_quarter_center_image = bgr_image[int(msg.height/4):int(msg.height* 3/4), int(msg.width/4):int(msg.width/2), 0]
+            right_quarter_center_image = bgr_image[int(msg.height/4):int(msg.height* 3/4), int(msg.width/2):int(msg.width*3/4), 0]
 
-        #             if np.all(np.logical_and(pixel >= lower, pixel <= upper)):
-        #                 color_counts[color] += 1
-        #                 # self.get_logger().info(f"color:{color} match pixel:{pixel}, lower:{lower}, upper:{upper}") 
+            left_half_of_image = bgr_image[:, 0:int(msg.width/2), 0]
+            right_half_of_image = bgr_image[:, int(msg.width/2):int(msg.width), 0]
 
-                    #here we have to see whether there is no blue
+            left_mid_line_of_image = left_half_of_image[int(msg.height/2), :]
+            right_mid_line_of_image = right_half_of_image[int(msg.height/2), :]
+
+
+
+            number_of_not_blue_left = 0
+            number_of_not_blue_right = 0
+
+            for pixel in left_mid_line_of_image:
+                if pixel < 25:
+                    number_of_not_blue_left += 1     
+
+            for pixel in right_mid_line_of_image:
+                if pixel < 25:
+                    number_of_not_blue_right += 1     
+
+
+            self.get_logger().info(f" bias_difference:{self.bias_difference}= {number_of_not_blue_right} - {number_of_not_blue_left} ")            
+            self.bias_difference = number_of_not_blue_left - number_of_not_blue_right    
+
+
+
+            # for row in center_image:
+            #     for pixel in row:
+            #         # Check if the pixel's color falls within any of the color ranges
+            #         for color, (lower, upper) in color_ranges.items():
+            #             # if cv2.inRange(np.array(pixel), np.array(lower), np.array(upper)):
+            #             #     color_counts[color] += 1
+
+            #             if np.all(np.logical_and(pixel >= lower, pixel <= upper)):
+            #                 color_counts[color] += 1
+            #                 # self.get_logger().info(f"color:{color} match pixel:{pixel}, lower:{lower}, upper:{upper}") 
                     
 
         # Determine the color that appears most frequently
@@ -107,7 +145,8 @@ class Eddie_laserscan(Node):
         if (self.state == 1):
             move_direction.angular.x = 0.0
             move_direction.angular.y = 0.0
-            move_direction.angular.z = 0.0
+            move_direction.angular.z = self.pivot_rate * self.bias_difference 
+            # move_direction.angular.z = 0.0 
             move_direction.linear.x = 0.5
             move_direction.linear.y = 0.0
             move_direction.linear.z = 0.0
@@ -123,10 +162,10 @@ class Eddie_laserscan(Node):
             move_direction.linear.x = 0.0
             move_direction.linear.y = 0.0
             move_direction.linear.z = 0.0
-            self.get_logger().info('publishing, state is"%d"' % self.state)
+            # self.get_logger().info('publishing, state is"%d"' % self.state)
             
             self.state=22
-            self.pivot_begin_time = int(round(time.time() * 1000))            
+            self.rotation_begin_time = int(round(time.time() * 1000))            
 
         # Yellow, 2-turn right
         elif(self.state == 22):
@@ -138,14 +177,14 @@ class Eddie_laserscan(Node):
             move_direction.linear.z = 0.0
             #turn 90 degrees right
             current_time_in_miliseconds = int(round(time.time() * 1000))
-            if (current_time_in_miliseconds - self.pivot_begin_time) < 5300:
+            if (current_time_in_miliseconds - self.rotation_begin_time) < self.rotation_duration:
+            # if (self.next_target_color != 0):
                 self.state=22
-                self.get_logger().info('difference in time is"%d"' % (current_time_in_miliseconds - self.pivot_begin_time))
+                # self.get_logger().info('difference in time is"%d"' % (current_time_in_miliseconds - self.rotation_begin_time))
                 
             else:
                 self.state=1 # start moving forward again     
-            self.get_logger().info('publishing, state is"%d"' % self.state)
-
+            # self.get_logger().info('publishing, state is"%d"' % self.state)
 
 
         # Red, 1-stop
@@ -156,10 +195,10 @@ class Eddie_laserscan(Node):
             move_direction.linear.x = 0.0
             move_direction.linear.y = 0.0
             move_direction.linear.z = 0.0
-            self.get_logger().info('publishing, state is"%d"' % self.state)
+            # self.get_logger().info('publishing, state is"%d"' % self.state)
             
             self.state=33
-            self.pivot_begin_time = int(round(time.time() * 1000))            
+            self.rotation_begin_time = int(round(time.time() * 1000))            
 
         # Red, 2-turn left
         elif(self.state == 33):
@@ -171,16 +210,43 @@ class Eddie_laserscan(Node):
             move_direction.linear.z = 0.0
             #turn 90 degrees left
             current_time_in_miliseconds = int(round(time.time() * 1000))
-            if (current_time_in_miliseconds - self.pivot_begin_time) < 5300:
+            if (current_time_in_miliseconds - self.rotation_begin_time) < self.rotation_duration:
+            # if (self.next_target_color != 0):
                 self.state=33
-                self.get_logger().info('difference in time is"%d"' % (current_time_in_miliseconds - self.pivot_begin_time))
+                # self.get_logger().info('difference in time is"%d"' % (current_time_in_miliseconds - self.rotation_begin_time))
                 
             else:
                 self.state=1 # start moving forward again     
-            self.get_logger().info('publishing, state is"%d"' % self.state)
+            # self.get_logger().info('publishing, state is"%d"' % self.state)
 
-     
-     
+        # Green, 1-stop
+        elif(self.state == 4):
+            move_direction.angular.x = 0.0
+            move_direction.angular.y = 0.0
+            move_direction.angular.z = 0.0
+            move_direction.linear.x = 0.0
+            move_direction.linear.y = 0.0
+            move_direction.linear.z = 0.0
+            # self.get_logger().info('publishing, state is"%d"' % self.state)
+            
+            self.state=44
+            self.rotation_begin_time = int(round(time.time() * 1000))            
+
+        # Green, 2-turn around
+        elif(self.state == 44):
+            move_direction.angular.x = 0.0
+            move_direction.angular.y = 0.0
+            move_direction.angular.z = 1.0
+            move_direction.linear.x = 0.0
+            move_direction.linear.y = 0.0
+            move_direction.linear.z = 0.0
+            #turn 90 degrees left
+            current_time_in_miliseconds = int(round(time.time() * 1000))
+            
+            self.state=44
+            # self.get_logger().info('difference in time is"%d"' % (current_time_in_miliseconds - self.rotation_begin_time))
+                
+            # self.get_logger().info('publishing, state is"%d"' % self.state)
 
         self.pub.publish(move_direction)
     
@@ -189,7 +255,16 @@ def main(args=None):
     eddie_laserscan = Eddie_laserscan()
     eddie_laserscan.state = 1
     eddie_laserscan.first_time = True
-    eddie_laserscan.pivot_begin_time = time.time()
+    eddie_laserscan.rotation_begin_time = time.time()
+    # pivot_degree is a small pivot while moving to set direction closer to the target
+    eddie_laserscan.pivot_rate = 0.001 
+    eddie_laserscan.bias_difference = 0 
+    eddie_laserscan.pivot_start = False 
+    eddie_laserscan.next_target_color = 0
+    eddie_laserscan.rotation_duration = 5300
+
+    
+
 
     rclpy.spin(eddie_laserscan)
 
